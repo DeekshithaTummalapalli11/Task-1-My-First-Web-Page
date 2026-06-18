858fee7816298b34f9df9e5435f772f2c60eab72
DeekshithaTummalapalli11 <tummalapallideekshith11@gmail.com>
2026-06-18 10:18:49 +0530

    Added Task 1 project

---
+++ Js/script.js
@@ -0,0 +1,21 @@
+// window.onload=function(){
+
+// localStorage.setItem(
+// "message",
+// "Welcome to my first webpage"
+// );
+
+
+// alert(
+// localStorage.getItem("message")
+// );
+
+// }
+
+
+localStorage.setItem(
+    "message",
+    "Welcome to my first webpage"
+);
+
+console.log(localStorage.getItem("message"));
